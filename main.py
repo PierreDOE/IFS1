4e35ec730f25fe56109df068ecb4d6da8c2ca0d9
PierreDOE <pierredulagon@gmail.com>
2025-01-06 14:11:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 Pierre DOERFLER, January 2025
 
 """
-from Flutter.flutter import Flutter
+from FLUTTER.flutter import Flutter
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
