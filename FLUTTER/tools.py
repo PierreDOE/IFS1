45092ff928f3a961a59f4fcf87510dadfb1f64f2
BatisteRiviere <batiste.riviere@univ-tlse3.fr>
2025-01-09 19:11:42 +0100

    Update tools.py
    plus jolie

--- FLUTTER/tools.py
+++ FLUTTER/tools.py
@@ -1,2 +1,25 @@
+import sys
+
+
+param = dict(
+GREY = "\033[1;30m",
+RED = "\033[1;31m",
+YELLOW = "\033[1;33m",
+BLUE = "\033[1;34m",
+CYAN = "\033[1;36m",
+GREEN = "\033[0;32m",
+RESET = "\033[0;0m",
+BOLD = "\033[;1m",
+REVERSE = "\033[;7m")
+
+
+def set_section(message, color='GREEN'):
+    sys.stdout.write(param[color])
+    line()
+    print(message)
+    line()
+    sys.stdout.write(param['RESET'])
+
+
 def line():
     print('=' * 50)
